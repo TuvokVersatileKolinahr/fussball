a34aed18bf64687e844f17bd179ea99bb36bd217
Wouter <tuvokki@gmail.com>
2015-12-30 21:50:46 +0100

    Count to WIN!

--- client/match/match.js
+++ client/match/match.js
@@ -15,6 +15,20 @@
           });
         }
 
+        this.scoreRed = 0;
+        this.scoreBlue = 0;
+
+        this.addGoal = (goal_id) => {
+          if (goal_id == $scope.current.teamRed)
+            this.scoreRed++;
+          if (goal_id == $scope.current.teamBlue)
+            this.scoreBlue++;
+          if (this.scoreRed == 7)
+            this.finish($scope.current.teamRed)
+          if (this.scoreBlue == 7)
+            this.finish($scope.current.teamBlue)
+        }
+
         this.finish = (win_id) => {
           Games.update({_id: $scope.current._id},
             {
