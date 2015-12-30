4e04a119549a303ecaf988501e9c6f4c1d7715a8
Wouter <tuvokki@gmail.com>
2015-12-30 21:12:59 +0100

    Winner indicator

--- client/match/match.js
+++ client/match/match.js
@@ -9,6 +9,12 @@
       controller: function($scope, $reactive, $state) {
         $reactive(this).attach($scope);
 
+        if ($scope.current) {
+          this.winners = Teams.findOne({
+            _id: $scope.current.winner
+          });
+        }
+
         this.finish = (win_id) => {
           Games.update({_id: $scope.current._id},
             {
