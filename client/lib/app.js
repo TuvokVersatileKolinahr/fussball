db4f1684357b799006bcf0b0f7bc0ca76231c829
Wouter <tuvokki@gmail.com>
2016-01-01 10:28:45 +0100

    Moved to make use of Meteor load order

---
+++ client/lib/app.js
@@ -0,0 +1,38 @@
+/* global Meteor, Template, Session, Mongo, Players, AmplifiedSession, angular */
+
+/*
+ * Files in the lib directory at the root of your application are loaded
+ * first. Files that match main.* are loaded after everything else.
+ *
+ * Files in subdirectories are loaded before files in parent directories,
+ * so that files in the deepest subdirectory are loaded first (after lib),
+ * and files in the root directory are loaded last (other than main.*).
+ *
+ * Within a directory, files are loaded in alphabetical order by filename.
+ *
+ * These rules stack, so that within lib, for example, files are still loaded
+ * in alphabetical order; and if there are multiple files named main.js, the
+ * ones in subdirectories are loaded earlier.
+ */
+
+let modulesToLoad = [
+  'angular-meteor',
+  'ui.router',
+  'accounts.ui'
+];
+
+angular.module('fussball', modulesToLoad);
+
+Accounts.ui.config({
+  passwordSignupFields: "USERNAME_ONLY"
+});
+
+angular.module('fussball').controller('MainController', ['$scope', function ($scope) {
+  $scope.test = "The fußball app";
+}]);
+
+angular.module('fussball').filter('ISODate', () => {
+  return (input) => {
+    return moment(input).format('MMMM Do YYYY, h:mm:ss a');
+  };
+});
